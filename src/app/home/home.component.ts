d48ee4932dc53df4b25a2d8d5200ac7c288c393e
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-02 01:33:48 +0100

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  profile = {
+    firstname: "haithem",
+    lastname: "mihoubi",
+    website: "haithem.com",
+    country: "tunisia",
+    age: 25
+  }
+  linkImage = "https://www.ideematic.com/wp-content/uploads/2020/07/flutter_logo.png"
+  titleStyle = "color:blue;"
+  number = 0
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
 
+  inc() {
+    this.number++
+  }
+
+  dec() {
+    this.number--
+  }
+
+  show() {
+    this.titleStyle = "color:red;font-size:20px"
+  }
+
+  hide() {
+    this.titleStyle = "color:green;font-size:40px"
+  }
 }
