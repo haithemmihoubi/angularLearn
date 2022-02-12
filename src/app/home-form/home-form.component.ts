258cf7091f5d07e16916dd622c74c55944904fce
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-12 16:41:38 +0100

    working with  forms

---
+++ src/app/home-form/home-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-form',
+  templateUrl: './home-form.component.html',
+  styleUrls: ['./home-form.component.css']
+})
+export class HomeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
